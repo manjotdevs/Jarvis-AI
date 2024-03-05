485c98ff28bf1452bb982321caea4683692d450d
artix <you@example.com>
2024-03-05 09:08:10 +0530

    other files

---
+++ REAMDE.md
@@ -0,0 +1,4 @@
+ sudo apt install python3 pip python3.11-dev 
+ sudo apt install portaudio19-dev 
+sudo apt install python-pyaudio-doc 
+
---
+++ variables.py
@@ -0,0 +1,15 @@
+websites = [["youtube", "https://www.youtube.com"], ["google", "https://www.google.com"],
+            ["facebook", "https://www.facebook.com"], ["twitter", "https://twitter.com"],
+            ["linkedin", "https://www.linkedin.com"], ["instagram", "https://instagram.com"],
+            ["chatgpt", "https://chatgpt.com"], ["wikipedia", "https://en.wikipedia.org"],
+            ["github", "https://github.com"], ["stackoverflow", "https://stackoverflow.com"],
+            ["reddit", "https://reddit.com"]]
+
+softwares = [["text editor", "gedit"], ["browser", "firefox"],
+             ["vs code", "code"], ["pycharm", "pycharm-community"],
+             ["terminal", "gnome-terminal"], ["vim", "gnome-terminal -e vim"],
+             ['file manager', 'nautilus']]
+
+#a = input("What would you like to do?\n")
+#import  os
+#os.system("firefox https://www.youtube.com/results?search_query=" + (a[2`]))
